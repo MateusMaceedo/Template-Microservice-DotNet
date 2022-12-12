54f08d36ec1eb1078965885506f19b2ab2f098e8
Mateus Macedo <mateusouza2014@live.com>
2022-12-12 14:02:57 -0300

    feat: Adicionando implementação do cache

---
+++ Microservice.Catalog.Domain/Interfaces/Cache/ICache.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Microservice.Catalog.Domain.Interfaces.Cache
+{
+    public interface ICache
+    {
+        Task<string> GetAsync(string key);
+        Task SetAsync(string key, string value);
+    }
+}
---
+++ Microservice.Catalog.Infra/Repositories/Cache.cs
@@ -0,0 +1,32 @@
+﻿using Microservice.Catalog.Domain.Interfaces.Cache;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Threading.Tasks;
+
+namespace Microservice.Catalog.Infra.Repositories
+{
+    public class Cache : ICache
+    {
+        private readonly IDistributedCache _cache;
+        private readonly DistributedCacheEntryOptions _options;
+        public Cache(IDistributedCache cache)
+        {
+            _cache = cache;
+            _options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(3600),
+                SlidingExpiration = TimeSpan.FromSeconds(1200)
+            };
+        }
+
+        public async Task<string> GetAsync(string key)
+        {
+            return await _cache.GetStringAsync(key);
+        }
+
+        public async Task SetAsync(string key, string value)
+        {
+            await _cache.SetStringAsync(key, value, _options);
+        }
+    }
+}
